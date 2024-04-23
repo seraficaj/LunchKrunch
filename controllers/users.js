2a5da95fb292c8a298d47abdb6bd71e0d3372295
Jason Serafica <seraficaj@gmail.com>
2024-04-23 13:38:02 -0700

    move Users controller functions to own file. rename controller functions to match RESTful conventions

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,28 @@
 const User = require("../models/User");
 
 module.exports = {
-    register
+    index,
+    create,
+    new: newUser,
+    delete: deleteUser
 }
 
-function register(req,res) {
-    User.register(new User({ email: req.body.email, username: req.body.username }), req.body.password, function (err, user) { 
-        if (err) { 
-            res.json({ success: false, message: "Your account could not be saved. Error: " + err }); 
-        } 
-        else { 
-            req.login(user, (er) => { 
-                if (er) { 
-                    res.json({ success: false, message: er }); 
-                } 
-                else { 
-                    res.json({ success: true, message: "Your account has been saved" }); 
-                } 
-            }); 
-        } 
-    }); 
+async function index(req, res) {
+    const allUsers = await User.find({});
+    res.json(allUsers);
+}
+
+async function create(req, res) {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+}
+
+function newUser(req, res,) {
+    res.render("signup", {title: "Sign Up"})
+}
+
+async function deleteUser(req, res) {
+    const deletedUsers = await User.deleteMany();
+    res.json(deletedUsers);
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const mongoose = require("mongoose");
 const User = require("../models/User");
+const usersCtrl = require("../controllers/users");
 
 /* GET users listing. */
-router.get("/", async function (req, res, next) {
-    const allUsers = await User.find({});
-    res.json(allUsers);
-});
+router.get("/", usersCtrl.index);
 
-router.get("/create", function (req, res, next) {
-    res.render("signup");
-});
+router.get("/new", usersCtrl.new);
 
-router.post("/create", async function (req, res, next) {
-    console.log(req.body);
-    const newUser = await User.create(req.body);
-    res.json(newUser);
-});
+router.post("/", usersCtrl.create);
 
-router.delete("/deleteUsers", async function (req, res, next) {
-    const deletedUsers = await User.deleteMany();
-    res.json(deletedUsers);
-})
+router.delete("/deleteUsers", usersCtrl.delete)
 
 module.exports = router;
